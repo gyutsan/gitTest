3f8eae803b19187a92c908c1ec7b487975791bf8
Yuning Nae <gyutsan@gmail.com>
2018-05-07 20:04:19 +0900

    Test the process call

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,43 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+
+            mCheckProcessTimer = new System.Timers.Timer();
+            mCheckProcessTimer.Interval = 1000;
+            mCheckProcessTimer.Elapsed += checkProcess;
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartProcess();
+
+            mCheckProcessTimer.Start();
+        }
+
+        public void StartProcess()
+        {
+            Process tempProcess = Process.Start(@"C:\Users\Yuning Nae\Documents\GitHub\gitTest\WindowsFormsApp1\testWinForms\bin\Debug\testWinForms");
+            mProcessId = tempProcess.Id;
+            textBox1.AppendText("Process Id : " + mProcessId + Environment.NewLine);
+        }
+
+        private void checkProcess(object sendor, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                Process tempProcess = Process.GetProcessById(mProcessId);
+                //Process.GetProcessById(mModuleInfoTable[i].moduleProcessId);
+                // 함수이름 가져오기 예제
+                //txtLog(MethodBase.GetCurrentMethod().Name + "hello~~");
+            }
+            catch
+            {
+                textBox1.AppendText(mProcessId + " is dead" + Environment.NewLine);
+                StartProcess();
+            }
+        }
+
+        private System.Timers.Timer mCheckProcessTimer = null;
+        private int mProcessId = 0;
     }
 }
---
+++ WindowsFormsApp1/testWinForms/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                textBox1.Text = i.ToString();
+            }
+
+            // 일부러 exception을 만듦.
+            String a = null;
+            a.ToString();
+        }
+    }
+}
